146a130a8cf025fe26e7537349047a9e29da9a46
Monique Hayes <moniqueehayes5@gmail.com>
2023-08-04 15:21:43 -0500

    cards created for each product. styling still needed

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchCatalogAsync, selectCatalog } from '../store/catalogSlice';
 import axios from 'axios';
 import catalogData from '../catalog-data';
-
+import {Card} from 'react-bootstrap'
 
 
 const Main = () => {
@@ -19,10 +19,30 @@ const Main = () => {
 
     const products = catalogData.products;
 
+    /// some of the products don't have an image, title, or author. need to make conditional
     return (
         <div>
-            <h1>Test</h1>
+            <div style={{textAlign: 'center'}}>
+            <h1>Edelwiess Catalog Viewer</h1>
+            <p>Browse our collection below!</p>
+            </div>
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            {products.map((product) => {
+                const jacketCov = product.images ? product.images['0']['uri'] : 'https://img.freepik.com/free-vector/vector-blank-book-cover-isolated-white_1284-41904.jpg?w=2000';
+                
+                return (
+                    <Card key={product.fullName + product.sku} style={{width: '40vw', margin: '2vh 3vw'}}>
+                        <Card.Img src={jacketCov} style={{}}/>
+                        <Card.Body>
+                            <Card.Title>{product.name ? product.name : 'Title Unknown'}</Card.Title>
+                            <Card.Text>By: {product.author ? product.author : 'Author Unknown'}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+            </div>
         </div>
+        
     )
 };
 
